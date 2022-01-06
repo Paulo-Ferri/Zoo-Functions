12c65178ee9357e486c401c02072e96ff08b3bd6
Paulo Afonso <pauloferrisouza@gmail.com>
2022-01-06 17:27:34 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, idade) {
+  const allSameAnimals = data.species.find((element) => element.name === animal);
+  const isHigher = allSameAnimals.residents.every((element) => element.age >= idade);
+  return isHigher;
 }
 
 module.exports = getAnimalsOlderThan;
