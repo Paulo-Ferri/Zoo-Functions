9bc402b4c60bacf329c9eaffd108f662edab773c
Paulo Afonso <pauloferrisouza@gmail.com>
2022-01-06 15:43:32 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    const arrVazio = [];
+    return arrVazio;
+  }
+  const newArr = [];
+  for (let index = 0; index < ids.length; index += 1) {
+    const species = data.species.find((element) => element.id === ids[index]);
+    newArr.push(species);
+  }
+  return newArr;
 }
 
 module.exports = getSpeciesByIds;
