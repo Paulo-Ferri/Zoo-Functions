3e9252d4b29c15a9a4fd4ef95ae5c8840cd0c1fd
Paulo Afonso <pauloferrisouza@gmail.com>
2022-01-10 13:21:13 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const allAnimals = data.species.reduce((acc, element) => {
+      acc[element.name] = element.residents.length;
+      return acc;
+    }, {});
+    return allAnimals;
+  }
+  const animalRef = data.species.find((element) =>
+    element.name === animal.specie);
+  if (Object.keys(animal).includes('sex')) {
+    const animalBySex = animalRef.residents.filter((element) =>
+      element.sex === animal.sex);
+    return animalBySex.length;
+  }
+  const quantidadeAnimais = animalRef.residents.length;
+  return quantidadeAnimais;
 }
 
 module.exports = countAnimals;
