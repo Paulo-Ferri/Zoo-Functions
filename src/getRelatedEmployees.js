66bafd3f766a8b7d899bd67980756450ec16231c
Paulo Afonso <pauloferrisouza@gmail.com>
2022-01-10 11:28:28 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const manager = data.employees.some((element) => element.managers.includes(id));
+  return manager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const manager = data.employees.filter((element) =>
+      element.managers.includes(managerId));
+    const colaboradores = manager.reduce((acc, element) =>
+      acc.concat(`${element.firstName} ${element.lastName}`), []);
+    return colaboradores;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
