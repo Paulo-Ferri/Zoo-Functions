3b88a8a6ce887fefcd38db4fa1212e10526b527a
Paulo Afonso <pauloferrisouza@gmail.com>
2022-01-12 23:06:53 -0300

    requisito 10 com lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function coverage(parametro) {
+  const dados = data.employees.find((element) => element.id === parametro.id
+  || element.firstName === parametro.name || element.lastName === parametro.name);
+  if (dados === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const idAnimais = dados.responsibleFor.map((element) => element);
+  const animaisReponsaveis = [];
+  for (let index = 0; index < idAnimais.length; index += 1) {
+    animaisReponsaveis.push(data.species.find((element) => element.id === idAnimais[index]));
+  }
+  const localizacoes = animaisReponsaveis.map((element) => element.location);
+  const nomeAnimaisReponsaveis = animaisReponsaveis.map((element) => element.name);
+  const resultado = { id: dados.id,
+    fullName: `${dados.firstName} ${dados.lastName}`,
+    species: nomeAnimaisReponsaveis,
+    locations: localizacoes,
+  };
+  return resultado;
+}
+
+function employees() {
+  const newArr = [];
+  data.employees.forEach((element) => newArr.push(coverage(element)));
+  return newArr;
+}
+
+function getEmployeesCoverage(parametro) {
+  if (!parametro) {
+    return employees();
+  }
+
+  return coverage(parametro);
 }
 
 module.exports = getEmployeesCoverage;
